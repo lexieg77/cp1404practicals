d23263441c90b3234033717b892f27a5e3818947
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-09-23 13:01:59 +1000

    Add random score generator and print the result

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,9 +2,17 @@
 Program to determine score status
 """
 
+import random
+
 def main():
     score = float(input("Enter score: "))
     print(determine_score_status(score))
+    print("\n")
+
+    # generates a random score and prints the results
+    random_score = random.randint(0, 100)
+    print(random_score)
+    print(determine_score_status(random_score))
 
 def determine_score_status(score):
     if score < 0 or score > 100:
