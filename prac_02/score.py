6c2514c83b0cbe77edbbd161872b0d6d3a8ba2cb
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-09-23 12:59:25 +1000

    Refactor score status program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,20 @@
 """
-CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(determine_score_status(score))
+
+def determine_score_status(score):
+    if score < 0 or score > 100:
+        status = "Invalid score"
+    elif score >= 90:
+        status = "Excellent"
+    elif score >= 50:
+        status = "Passable"
+    else:
+        status = "Bad"
+    return status
+
+main()
