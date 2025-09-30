6add676a1489c3f490090c381b85e856210565fc
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-09-30 12:35:04 +1000

    Add comments for each question

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+# What did you see on line 1?
+# Printed a random integer between 5 and 20
+
+# What was the smallest number you could have seen, what was the largest?
+# The smallest possible is 5 and the largest possible is 20
+
+
+# What did you see on line 2?
+# Printed a random odd number between 3 and 10
+
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number was 3 and the largest was 9
+
+# Could line 2 have produced a 4?
+# No as it only printed odd numbers between 3 and 10
+
+# What did you see on line 3?
+# Printed a random decimal between 2.5 and 5.5
+
+# What was the smallest number you could have seen, what was the largest?
+# The smallest would have been 2.5 and the largest 5.5
