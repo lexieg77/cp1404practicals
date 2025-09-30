5f66b79ca597b57cbe1b69568da75532ad3dc0e0
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-09-30 13:34:12 +1000

    Add questions 1 to 3

---
+++ prac_03/files.py
@@ -0,0 +1,19 @@
+# Q1.
+# user_name = input("Enter your name: ")
+# out_file = open("name.txt", 'w')
+# print(user_name, file = out_file)
+# out_file.close()
+
+# Q2.
+out_file = open("name.txt", 'r')
+name = out_file.readline().strip()
+print(f"Hi {name}!")
+out_file.close()
+
+# Q3.
+with open("numbers.txt", 'r') as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+    number_addition = first_number + second_number
+print(f"The addition of {first_number} and {second_number} is {number_addition}")
+
