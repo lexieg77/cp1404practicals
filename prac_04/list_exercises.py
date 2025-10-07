52eaffeb1c2e99b9d55a6c01de628682670d75e3
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-07 13:07:36 +1000

    Add print statements

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,3 +4,9 @@ for i in range(5):
     user_input = int(input("Number: "))
     numbers.append(user_input)
 print(numbers)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {(sum(numbers)/len(numbers))}")
