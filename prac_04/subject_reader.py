912423a9ceff035ba461a02a8dcde413b6fdfc2d
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-07 12:56:54 +1000

    Add meaningful names

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -5,29 +5,28 @@ Data file -> lists program
 
 FILENAME = "subject_data.txt"
 
+
 def main():
-    data = load_data(FILENAME)
-    display_subject_details(data)
+    subjects = load_data(FILENAME)
+    display_subject_details(subjects)
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
-    data = []
+    subject = []
     input_file = open(filename)
     for line in input_file:
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        data.append(parts)
+        subject.append(parts)
     input_file.close()
-    return data
+    return subject
 
-def display_subject_details(data):
-    for subject in data:
+
+def display_subject_details(subjects):
+    for subject in subjects:
         print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
-
-
-
 
 
 main()
