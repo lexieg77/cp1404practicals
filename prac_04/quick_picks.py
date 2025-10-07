f47a0b1d2f53d9d2faed5f94c99458f21a092205
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-07 20:09:01 +1000

    Add for loop to generate the rows of six numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,3 +4,17 @@ NUMBERS_PER_LINE = 6
 MIN_NUMBER = 1
 MAX_NUMBER = 45
 
+def main():
+    number_of_quick_picks = int(input("How many quick picks would you like? "))
+
+    for i in range(number_of_quick_picks):
+        numbers = []
+        for j in range(NUMBERS_PER_LINE + 1):
+            number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            if number not in numbers:
+                numbers.append(number)
+
+        quick_pick = sorted(numbers)
+
+
+main()
