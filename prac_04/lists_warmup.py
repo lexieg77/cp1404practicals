ae5c7ee4b02a48f2fdaa1795f19ba2c471e7bce0
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-07 12:15:15 +1000

    Add extra statements

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -8,3 +8,18 @@ print(5 in numbers) # True
 print(7 in numbers) # False
 print("3" in numbers) # False
 print(numbers + [6, 5, 3]) # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# change the first element to "ten"
+numbers[0] = "ten"
+print(numbers)
+
+# change the last number to 1
+numbers[-1] = 1
+print(numbers)
+
+# print all but the first two elements
+print(numbers[2:])
+
+# print whether 9 is an element of numbers
+print(9 in numbers)
+
