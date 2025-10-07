668fbefb13086b4fa27ab17da62f607ecd166d51
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-07 20:21:23 +1000

    Add basic code for handling indefinite strings

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,38 +1,46 @@
-numbers = []
+# numbers = []
+#
+# # 1. Basic list operations
+# for i in range(5):
+#     user_input = int(input("Number: "))
+#     numbers.append(user_input)
+# print(numbers)
+#
+# print(f"The first number is {numbers[0]}")
+# print(f"The last number is {numbers[-1]}")
+# print(f"The smallest number is {min(numbers)}")
+# print(f"The largest number is {max(numbers)}")
+# print(f"The average of the numbers is {(sum(numbers)/len(numbers))}")
+#
+# # 2. Woefully inadequate security checker
+# usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+#              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+#
+# username = input("Enter your username: ")
+# if username in usernames:
+#     print("Access granted")
+# else:
+#     print("Access denied")
+#
+# # 3. Extend basic list operations
+# i = 1
+# user_input = int(input(f"Number {i}: "))
+# while user_input != 0:
+#     numbers.append(user_input)
+#     i = i + 1
+#     user_input = int(input(f"Number {i}: "))
+# print(numbers)
+#
+# print(f"The first number is {numbers[0]}")
+# print(f"The last number is {numbers[-1]}")
+# print(f"The smallest number is {min(numbers)}")
+# print(f"The largest number is {max(numbers)}")
+# print(f"The average of the numbers is {(sum(numbers)/len(numbers))}")
 
-# 1. Basic list operations
-for i in range(5):
-    user_input = int(input("Number: "))
-    numbers.append(user_input)
-print(numbers)
-
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {(sum(numbers)/len(numbers))}")
-
-# 2. Woefully inadequate security checker
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-
-username = input("Enter your username: ")
-if username in usernames:
-    print("Access granted")
-else:
-    print("Access denied")
-
-# 3. Extend basic list operations
-i = 1
-user_input = int(input(f"Number {i}: "))
-while user_input != 0:
-    numbers.append(user_input)
-    i = i + 1
-    user_input = int(input(f"Number {i}: "))
-print(numbers)
-
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {(sum(numbers)/len(numbers))}")
+# 4. Indefinite set of strings
+set_of_strings = []
+user_input = input("Enter a string: ")
+while user_input != "":
+    set_of_strings.append(user_input)
+    user_input = input("Enter a string: ")
+print(set_of_strings)
