39565eec14fd6d1cccad35c01fb39493045c32c9
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-14 15:53:27 +1000

    Add function to display the champions and countries for wimbledon week 5 prac

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,33 +3,41 @@ INDEX_COUNTRY = 1
 INDEX_CHAMPION = 2
 
 def main():
-    records = load_data(FILENAME)
-    champion_to_court, countries = process_champions_and_countries(records)
+    """ Read the records in the file and output details about the Wimbledon champions and countries"""
+    records = load_records(FILENAME)
+    champion_to_count, countries = process_records(records)
+    display_results(champion_to_count, countries)
 
+def process_records(records):
+    """ Process the records to create a dictionary of champions and countries. """
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPION]] = 1
+    return champion_to_count, countries
 
-def load_data(filename):
+def display_results(champion_to_count, countries):
+    """ Display champions and countries. """
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
+
+def load_records(filename):
     """ Load the records from the file into a list of lists. """
     records = []
 
     with open(filename, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline() # remove column headings
-        lines = in_file.readlines()
-        for line in lines:
+        in_file.readline()  # Remove CSV header row
+        for line in in_file:
             parts = line.strip().split(",")
             records.append(parts)
-
-def process_champions_and_countries(records):
-    """ Process the records to create a dictionary of champions and countries """
-    champion_to_count = {}
-    countries = set()
-
-    for record in records:
-        countries.add[record[INDEX_COUNTRY]] += 1
-        try:
-            champion_to_count[record[INDEX_CHAMPION] += 1
-        except KeyError:
-            champion_to_count[INDEX_CHAMPION] = 1
-    return champion_to_count, countries
+    return records
 
 
 main()
