8176d903342d6c82b53cfe8b0573a68bde43c9bb
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-14 12:46:16 +1000

    Add code to display hex for an inputted colour

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,6 +1,12 @@
 
-HEX_TO_COLOUR = {"#0048ba": "AbsoluteZero", "#fbceb1": "Apricot", "b2beb5": "AshGrey", "9966cc": "Amethyst",
-                 "bfff00": "BitterLime", "#fe6f5e": "Bittersweet", "ffe135": "BananaYellow", "d19fe8": "BrightUbe",
-                 "c19a6b": "Camel", "b2ffff": "Celeste"}
+COLOURS_TO_HEX = {"absolutezero": "#0048ba", "apricot": "#fbceb1", "ashgrey": "b2beb5", "amethyst": "9966cc",
+                 "bitterlime": "bfff00", "bittersweet": "#fe6f5e", "bananayellow": "ffe135", "brightube": "d19fe8",
+                 "camel": "c19a6b", "celeste": "b2ffff"}
+
+
+colour_name = input("Enter a colour name: ").lower()
+while colour_name != "":
+    print(f"The code for \"{colour_name}\" is {COLOURS_TO_HEX.get(colour_name)}")
+    colour_name = input("Enter a colour name: ").lower()
 
 
