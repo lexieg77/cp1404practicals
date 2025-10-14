56830d6724027d065601cd5b5f5ed8d3060dcaae
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-14 12:37:06 +1000

    Add hex and colour dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,6 @@
+
+HEX_TO_COLOUR = {"#0048ba": "AbsoluteZero", "#fbceb1": "Apricot", "b2beb5": "AshGrey", "9966cc": "Amethyst",
+                 "bfff00": "BitterLime", "#fe6f5e": "Bittersweet", "ffe135": "BananaYellow", "d19fe8": "BrightUbe",
+                 "c19a6b": "Camel", "b2ffff": "Celeste"}
+
+
