561c415d22bbe56088ea2b53ef1c9ac808154701
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-14 12:59:29 +1000

    Add display to show the number of repeated words in the string

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -10,9 +10,11 @@ words = input("Please enter a string of words: ").split(" ")
 for word in words:
     frequency = word_to_count.get(word, 0)
     word_to_count[word] = frequency + 1
-    print(word_to_count[word])
 
-# keys = words, values = counts
+max_length = max(len(word) for word in words)
+for word in sorted(word_to_count):
+    print(f"{word:{max_length}} : {word_to_count[word]}")
+
 
 
 
