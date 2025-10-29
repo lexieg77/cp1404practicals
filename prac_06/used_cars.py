1ea957ffe4466a3af535c2357d04c7ebafd41128
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-29 14:30:29 +1000

    Use __str__ method from car.py

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -10,14 +10,15 @@ from prac_06.car import Car
 def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
+    my_car.name = "Car"
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
     limo = Car(100)
+    limo.name = "Limo"
     limo.add_fuel(20)
-    print(f"Fuel in the limo: {limo.fuel}")
     limo.drive(115)
+    print(limo)
 
 
 main()
