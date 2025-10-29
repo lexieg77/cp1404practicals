92915526be04f374a88c9feea25d0982c7f02345
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-29 15:33:07 +1000

    Add in print statement for if the guitar is vintage and how many guitars are in the list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,9 @@
+
 from prac_06.guitar import Guitar
 
+
 def main():
+    """Guitar program, using Guitar class."""
     guitars = []
 
     print("My guitars!")
@@ -8,14 +11,24 @@ def main():
     while name != "":
         year = int(input("Year: "))
         cost = float(input("Cost: $"))
-        new_guitar = Guitar(name, year, cost)
-        guitars.append(new_guitar)
-        print(new_guitar, "added.")
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
         name = input("Name: ")
 
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            # Note the use of the format method and numbered placeholders
+            print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
+    else:
+        print("No guitars :( Quick, go and buy one!")
 
 
 main()
