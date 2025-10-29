1fc002eb24f8545502dff330b67144fd5e6dd5db
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-10-29 15:30:07 +1000

    Add user input for a guitars name, year and cost

---
+++ prac_06/guitars.py
@@ -0,0 +1,21 @@
+from prac_06.guitar import Guitar
+
+def main():
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar, "added.")
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+
+
+main()
