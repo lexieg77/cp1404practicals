f3a5f81343fafe907e5aa38a31fc4f282b04e86b
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-04 12:59:54 +1000

    Add guitar.py code form prac 06

---
+++ prac_07/guitar.py
@@ -0,0 +1,22 @@
+CURRENT_YEAR = 2017
+VINTAGE_AGE = 50
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0, age=0):
+        """ Represent guitar information. """
+        self.name = name
+        self.year = year
+        self.cost = cost
+        self.age = age
+
+    def __str__(self):
+        """ Return name, year and cost. """
+        return f"{self.name} ({self.year}) : {self.cost}"
+
+    def get_age(self):
+        """ Returns the age of the guitar. """
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """ Returns if the guitar is vintage or not. """
+        return self.get_age() >= VINTAGE_AGE
