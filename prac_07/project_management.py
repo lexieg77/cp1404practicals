1fe279642703c516695c1d7ca545161b2358fbf4
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-08 15:59:51 +1000

    Add MENU for project management code

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,14 +1,51 @@
 FILENAME = "projects.txt"
 
+MENU = """
+ - (L)oad projects
+ - (S)ave projects
+ - (D)isplay projects
+ - (F)ilter projects by date
+ - (A)dd new project
+ - (U)pdate project
+ - (Q)uit
+"""
+
 from prac_07.project import Project
 
 def main():
-    filename = input("Please enter a file name to load projects from: ")
-    projects = load_data_file(filename)
-    print(projects)
 
-def load_data_file(filename):
-    """Load data from file name."""
+    print("Welcome to Pythonic Project Management")
+    projects = load_projects(FILENAME)
+    print(f"Loaded {len(projects)} projects from {FILENAME}.")
+
+    # Menu
+    print(MENU)
+    choice = input(">>> ").upper()
+
+    # Menu options
+    while choice != "Q":
+        if choice == "L":
+            filename = input("Please enter a file name to load projects from: ")
+            projects = load_projects(filename)
+            print(projects)
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+
+
+def load_projects(filename):
+    """Load project data form user input."""
     projects = []
     with open(filename, 'r') as in_file:
         in_file.readline()
@@ -16,5 +53,9 @@ def load_data_file(filename):
             clean_line = line.replace("\t", " ").replace("\n", "")
             projects.append(clean_line)
         return projects
+
+def save_projects():
+    filename = input("Enter a filename to save your projects to ")
+
 
 main()
