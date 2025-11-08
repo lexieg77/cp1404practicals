6fe8a5e73572d8901d994cb6c58e7cfe31978318
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-08 15:30:57 +1000

    Add functions for each step in the guitars program

--- prac_07/my_guitars.py
+++ prac_07/my_guitars.py
@@ -6,29 +6,10 @@ import csv
 def main():
     """Guitar program, using Guitar class."""
     guitars = reading_csv_file()
+    sort_guitars(guitars)
+    add_new_guitar(guitars)
+    write_new_guitars(guitars)
 
-    # Sort guitars by year
-    guitars.sort()
-    print("Guitars sorted by year:")
-    for guitar in guitars:
-        print(guitar)
-
-    # Add new guitars
-    print("Add a guitar")
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        guitar_to_add = Guitar(name, year, cost)
-        guitars.append(guitar_to_add)
-        print(f"{guitar_to_add} added.")
-        name = input("Name: ")
-
-    # Write all guitars back to the CSV file
-    with open(FILENAME, "w", newline='') as out_file:
-        writer = csv.writer(out_file)
-        for guitar in guitars:
-            writer.writerow([guitar.name, guitar.year, guitar.cost])
 
 def reading_csv_file():
     """Read guitars from a CSV file and return a list of Guitar objects."""
@@ -40,5 +21,32 @@ def reading_csv_file():
                 name, year, cost = row
                 guitars.append(Guitar(name, int(year), float(cost)))
     return guitars
+
+def sort_guitars(guitars):
+    """ Sort guitars by year."""
+    guitars.sort()
+    print("Guitars sorted by year:")
+    for guitar in guitars:
+        print(guitar)
+
+def add_new_guitar(guitars):
+    """Add new guitars to the csv file."""
+    print("Add a guitar")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(f"{guitar_to_add} added.")
+        name = input("Name: ")
+
+def write_new_guitars(guitars):
+    """Write new guitars to the CSV file."""
+    with open(FILENAME, "w", newline='') as out_file:
+        writer = csv.writer(out_file)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
+
 
 main()
