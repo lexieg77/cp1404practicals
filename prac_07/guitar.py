dd7166ee4a8a39153c29af6e3da61c6772b52747
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-08 15:27:20 +1000

    Complete adding guitars to the csv file

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -24,3 +24,6 @@ class Guitar:
 
     def __lt__(self, other):
         return self.year < other.year
+
+    def add_to_list(self):
+        return [self.name, self.year, self.cost]
--- prac_07/my_guitars.py
+++ prac_07/my_guitars.py
@@ -1,3 +1,4 @@
+FILENAME = "guitars.csv"
 
 from prac_07.guitar import Guitar
 import csv
@@ -12,18 +13,32 @@ def main():
     for guitar in guitars:
         print(guitar)
 
+    # Add new guitars
+    print("Add a guitar")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(f"{guitar_to_add} added.")
+        name = input("Name: ")
+
+    # Write all guitars back to the CSV file
+    with open(FILENAME, "w", newline='') as out_file:
+        writer = csv.writer(out_file)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
 
 def reading_csv_file():
-    """Language file reader version using the csv module."""
-    in_file = open('guitars.csv', 'r', newline='')
-    in_file.readline()
-    reader = csv.reader(in_file)
+    """Read guitars from a CSV file and return a list of Guitar objects."""
     guitars = []
-    for row in reader:
-        guitars.append(row)
-    in_file.close()
-
+    with open(FILENAME, 'r', newline='') as in_file:
+        reader = csv.reader(in_file)
+        for row in reader:
+            if len(row) == 3:  # skip bad rows
+                name, year, cost = row
+                guitars.append(Guitar(name, int(year), float(cost)))
     return guitars
-
 
 main()
