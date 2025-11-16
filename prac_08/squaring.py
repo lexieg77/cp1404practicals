06a26ce3fcb11cdb7e60c056756ec0a6dff7bc75
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-16 13:20:11 +1000

    Update window size for the squaring app

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -4,17 +4,17 @@ from kivy.core.window import Window
 
 
 class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+    """SquareNumberApp is a Kivy App for squaring a number."""
 
     def build(self):
-        """ build the Kivy app from the kv file """
+        """Bild the Kivy app from the kv file."""
         Window.size = (300, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """Handle calculation (could be button press or other call), output result to label widget."""
         try:
             result = float(value) ** 2
             self.root.ids.output_label.text = str(result)
