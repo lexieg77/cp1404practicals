59802d89b42ee273ac22483e24c64c4c00a85584
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-16 13:43:09 +1000

    Add handling for string property

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,9 +1,12 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 class ConvertMilesKmApp(App):
     """ConvertMilesKmApp is a Kivy App for converting miles to km."""
+
+    output = StringProperty()
 
     def build(self):
         """Bild the Kivy app from the kv file."""
@@ -12,5 +15,10 @@ class ConvertMilesKmApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
+
+
+    def handle_conversion(self):
+        """Handle changes to the text input by updating the model from the view."""
+        self.output = self.root.ids.input_number
 
 ConvertMilesKmApp().run()
