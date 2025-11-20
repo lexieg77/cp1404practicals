8bfddf1895c9699841de8255afc68ab2a65b4bf7
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-20 13:35:03 +1000

    Add function to test an unreliable car of 30% multiple times

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -3,4 +3,10 @@ from unreliable_car import UnreliableCar
 def main():
     """Testing an unreliable car of 30%"""
     my_car = UnreliableCar("Prius 1", 30, 30)
-    print(f"My car is able to drive {my_car.drive(20)} km")
+
+    for i in range(1, 15):
+        print(f"My car is able to drive {my_car.drive(i)} km")
+
+    print(my_car)
+
+main()
