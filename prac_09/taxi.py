b1677c65deb474446d2a3700da495c849f512ee6
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-22 16:17:26 +1000

    Add get fare, start fare and drive to taxi class

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -14,4 +14,17 @@ class Taxi(Car):
         """Return a string like a Car but with current fare distance."""
         return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
+    def get_fare(self):
+        """Return the price for the taxi trip rounded."""
+        fare = self.price_per_km * self.current_fare_distance
+        return fare
 
+    def start_fare(self):
+        """Begin a new fare."""
+        self.current_fare_distance = 0
+
+    def drive(self, distance):
+        """Drive like parent Car but calculate fare distance as well."""
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
