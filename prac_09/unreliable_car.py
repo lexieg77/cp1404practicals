7b263a380bcd716cbdfdb43703b04301062f1b7d
lexieg77 <lexie.giandon@my.jcu.edu.au>
2025-11-22 16:18:26 +1000

    Initialise unreliable car class

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -6,13 +6,7 @@ class UnreliableCar(Car):
     """Initialising UnreliableCar class using the Car class."""
 
     def __init__(self, name: str, fuel: int, reliability: float):
+        """Initialise unreliable car class"""
         super().__init__(name, fuel)
         self.reliability = reliability
 
-    def drive(self, distance):
-        """Driving the car if the reliability is big enough."""
-        random_num = randint(0, 100)
-        if random_num >= self.reliability:
-            distance = 0
-        driving_distance = super().drive(distance)
-        return driving_distance
